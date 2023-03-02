96d25fc7c3d315f260718bce41a3d8e581dd9e00
Abdullah <53242416+git-abdullah@users.noreply.github.com>
2023-03-02 15:00:26 +0500

    done.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # simple-blog-template-2
  
+## Screenshots
+![shot1](demo/1.png)
+![shot2](demo/2.png)
+## Build
+`npx tailwindcss -i public/css/input.css -o public/css/styles.css --minify  --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-10': '#A7727D',
+        'primary-30': '#EDDBC7',
+        'primary-60': '#F9F5E7'
+      }
+    },
+  },
+  plugins: [],
+}
